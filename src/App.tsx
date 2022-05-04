f1a1e1148fb113da024e05298cf2a270a9898ea6
R Lee <alpha.r.lee.dev@gmail.com>
2022-05-04 04:08:19 -0600

    First commit - I have no idea how to use TS

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+function App() {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyCoggmLOqJa2LpyRiUGvdAeoLYtH445Vws",
+  //   authDomain: "shplub-4dc44.firebaseapp.com",
+  //   projectId: "shplub-4dc44",
+  //   storageBucket: "shplub-4dc44.appspot.com",
+  //   messagingSenderId: "814937975809",
+  //   appId: "1:814937975809:web:d7694a08ac97342c982400",
+  //   measurementId: "G-F2C4GDPRW7",
+  // };
+
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
+
+  return <h3>Hello world! Typescript time!</h3>;
+}
+
+export default App;
